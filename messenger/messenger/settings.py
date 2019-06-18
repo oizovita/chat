97ea1164b5b2d4ea414c029337e73233443ccc27
oizovita <oles1103@gmail.com>
2019-06-18 20:38:31 +0300

    update setting

--- messenger/messenger/settings.py
+++ messenger/messenger/settings.py
@@ -140,7 +140,3 @@ LOGIN_REDIRECT_URL = '/chat/global'
 
 #ACCOUNT_ACTIVATION_DAYS = 7
 
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
